855a760e0ffc5ad24afcfd1148b60cfa6d8b5879
Rodrigo3R <rodrigo3r7@hotmail.com>
2020-06-09 09:22:09 -0300

    Criacao de listas

--- src/senai/Principal.java
+++ src/senai/Principal.java
@@ -1,9 +1,17 @@
 package senai;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Principal {
 
+	static List<String> funcionarios = new ArrayList<String>();
+	static List<String> clientes = new ArrayList<String>();
+	static List<String> servicos = new ArrayList<String>();
+	static List<String> vendas = new ArrayList<String>();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
