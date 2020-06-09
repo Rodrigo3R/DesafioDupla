9208a806c7901cb4ce3be29231ee91eb6fa9cfdd
Rodrigo3R <rodrigo3r7@hotmail.com>
2020-06-09 09:41:08 -0300

    Alteracao da listas

--- src/senai/Principal.java
+++ src/senai/Principal.java
@@ -3,16 +3,29 @@ package senai;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 public class Principal {
 
-	static List<String> funcionarios = new ArrayList<String>();
-	static List<String> clientes = new ArrayList<String>();
+	static List<String> nomeFuncionarios = new ArrayList<String>();
+	static List<String> setorFuncionarios = new ArrayList<String>();
+	static List<String> profissaoFuncionarios = new ArrayList<String>();
+	static List<String> nomeClientes = new ArrayList<String>();
+	static List<String> telefoneClientes = new ArrayList<String>();
 	static List<String> servicos = new ArrayList<String>();
 	static List<String> vendas = new ArrayList<String>();
 	
 	public static void main(String[] args) {
 	
 
+	}
+	
+
+	public static Integer jopInt(String mensagem) {
+
+		Integer numero = 0;
+		numero = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
+		return numero;
 	}
 
 }
