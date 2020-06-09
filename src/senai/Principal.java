54581dea0d4c1754c268ef8efafa324c110179a5
Rodrigo3R <rodrigo3r7@hotmail.com>
2020-06-09 11:18:58 -0300

    Lista de funcionarios

--- src/senai/Principal.java
+++ src/senai/Principal.java
@@ -15,17 +15,18 @@ public class Principal {
 	static List<String> ValorServicos = new ArrayList<String>();
 	static List<String> vendas = new ArrayList<String>();
 	static List<String> valorVenda = new ArrayList<String>();
-	
+
 	public static void main(String[] args) {
-	
+
 		StringBuffer menu = new StringBuffer();
-		
-		String nomeFuncionario = "";		
-		String profissaoFuncionario = "";		
+		StringBuffer lista = new StringBuffer();
+
+		String nomeFuncionario = "";
+		String profissaoFuncionario = "";
 		int resposta = -1;
-		
-		while( resposta != 0) {
-		
+
+		while (resposta != 0) {
+
 			menu = new StringBuffer();
 			menu.append("1 - Cadastrar funcionario\n");
 			menu.append("2 - Alterar funcionario\n");
@@ -39,106 +40,120 @@ public class Principal {
 			menu.append("10 - Efetuar venda\n");
 			menu.append("11 - Listar vendas\n");
 			menu.append("0- Sair\n");
-			
+
 			resposta = jopInt(menu.toString());
-			
+
 			switch (resposta) {
-			case 1: //  1 - Cadastrar funcionario
-				
+			case 1: // 1 - Cadastrar funcionario
+
 				nomeFuncionario = jopStr("Digite o nome do funcionário:");
 				profissaoFuncionario = jopStr("Digite a profissão  do funcionário:");
-				
+
 				nomeFuncionarios.add(nomeFuncionario);
 				profissaoFuncionarios.add(profissaoFuncionario);
 				jop("Funcionário cadastrado com sucesso");
-				
+
 				break;
 			case 2: // 2 - Alterar funcionario
-				
+
 				if (!nomeFuncionarios.isEmpty()) {
-					
+
 					nomeFuncionario = jopStr("Digite o nome do funcionário que deseja alterar:");
 					for (int i = 0; i < nomeFuncionarios.size(); i++) {
-						
-						if(nomeFuncionarios.get(i).equalsIgnoreCase(nomeFuncionario)) {
-							
+
+						if (nomeFuncionarios.get(i).equalsIgnoreCase(nomeFuncionario)) {
+
 							nomeFuncionario = jopStr("Digite o novo nome do funcionário:");
 							nomeFuncionarios.set(i, nomeFuncionario);
-							
-							profissaoFuncionario =jopStr("Deseja alterar a profissão? ( S / N )");
-						
-							if(profissaoFuncionario.equalsIgnoreCase("s")) {
-								
+
+							profissaoFuncionario = jopStr("Deseja alterar a profissão? ( S / N )");
+
+							if (profissaoFuncionario.equalsIgnoreCase("s")) {
+
 								profissaoFuncionario = jopStr("Digite a profissão do funcionário:");
 								profissaoFuncionarios.set(i, profissaoFuncionario);
-							
+
 							}
+
 							jop("Funcionário alterado com sucesso");
+
 						}
-					} 
-					
-				}
-				else {
+					}
+
+				} else {
 					jop("Lista vazia");
 				}
-				
+
 				break;
 			case 3: // 3 - Listar funcionarios
-				
+
+				if (!nomeFuncionarios.isEmpty()) {
+					lista = new StringBuffer();
+
+					for (int i = 0; i < nomeFuncionarios.size(); i++) {
+						lista.append(nomeFuncionarios.get(i) + "\n");
+					}
+
+					jop(lista.toString());
+
+				} else {
+					jop("Lista vazia");
+				}
+
 				break;
 			case 4: // 4 - Cadastrar cliente
-				
+
 				break;
 			case 5: // 5 - Alterar cliente
-				
+
 				break;
 			case 6: // 7 - Cadastrar serviço
-				
+
 				break;
 			case 7: // 7 - Cadastrar serviço
-				
+
 				break;
-			case 8: //8 - Alterar serviço
-				
+			case 8: // 8 - Alterar serviço
+
 				break;
 			case 9: // 9 - Listar serviço
-				
+
 				break;
 			case 10: // 10 - Efetuar venda
-				
+
 				break;
 			case 11: // 11 - Listar vendas
-				
+
 				break;
-			
+
 			case 0: // Sair
-				
-				resposta = JOptionPane.showConfirmDialog(null,"Tem certeza que deseja sair?");
-				
+
+				resposta = JOptionPane.showConfirmDialog(null, "Tem certeza que deseja sair?");
+
 				break;
 
 			default: // opção invalida
 				break;
 			}
-			
+
 		}
 
 	}
+
 	public static String jopStr(String mensagem) {
 
 		String retorno = "";
 		return JOptionPane.showInputDialog(mensagem);
-		
+
 	}
-	
 
 	public static final Integer jopInt(String mensagem) {
 
 		Integer numero = 0;
 		return Integer.parseInt(JOptionPane.showInputDialog(mensagem));
-		
+
 	}
-	
+
 	public static final void jop(String mensagem) {
 
 		JOptionPane.showMessageDialog(null, mensagem);
